6f67c4d1b176d6ae3f24c4b61a1189bca73aea54
Steven G. Johnson <stevenj@alum.mit.edu>
2020-08-15 16:28:44 -0400

    added gradient and Jacobian

--- src/FastChebInterp.jl
+++ src/FastChebInterp.jl
@@ -14,7 +14,7 @@ via `c(y)`.
 """
 module FastChebInterp
 
-export chebpoints, chebinterp
+export chebpoints, chebinterp, chebjacobian, chebgradient
 
 using StaticArrays
 import FFTW
@@ -144,7 +144,7 @@ function interpolate(x::SVector{N}, c::Array{<:Any,N}, ::Val{dim}, i1, len) wher
     if dim == 1
         c₁ = c[i1]
         if n ≤ 2
-            n == 1 && return c₁ + xd * zero(c₁)
+            n == 1 && return c₁ + one(xd) * zero(c₁)
             return c₁ + xd*c[i1]
         end
         @inbounds bₖ = c[i1+(n-2)] + 2xd*c[i1+(n-1)]
@@ -163,7 +163,7 @@ function interpolate(x::SVector{N}, c::Array{<:Any,N}, ::Val{dim}, i1, len) wher
 
         c₁ = interpolate(x, c, dim′, i1, Δi)
         if n ≤ 2
-            n == 1 && return c₁ + xd * zero(c₁)
+            n == 1 && return c₁ + one(xd) * zero(c₁)
             c₂ = interpolate(x, c, dim′, i1+Δi, Δi)
             return c₁ + xd*c₂
         end
@@ -193,5 +193,110 @@ end
 
 (interp::ChebInterp{N})(x::AbstractVector{<:Real}) where {N} = interp(SVector{N}(x))
 (interp::ChebInterp{1})(x::Real) = interp(SVector{1}(x))
+
+
+"""
+    Jinterpolate(x, c::Array{T,N}, ::Val{dim}, i1, len)
+
+Similar to `interpolate` above, but returns a tuple `(v,J)` of the
+interpolated value `v` and the Jacobian `J` with respect to `x[1:dim]`.
+"""
+function Jinterpolate(x::SVector{N}, c::Array{<:Any,N}, ::Val{dim}, i1, len) where {N,dim}
+    n = size(c,dim)
+    @inbounds xd = x[dim]
+    if dim == 1
+        c₁ = c[i1]
+        if n ≤ 2
+            n == 1 && return c₁ + one(xd) * zero(c₁), hcat(SVector(zero(c₁) / oneunit(xd)))
+            return c₁ + xd*c[i1], hcat(SVector(c[i1]))
+        end
+        @inbounds cₙ₋₁ = c[i1+(n-2)]
+        @inbounds cₙ = c[i1+(n-1)]
+        bₖ = cₙ₋₁ + xd*(bₖ′ = 2cₙ)
+        bₖ₊₁ = oftype(bₖ, cₙ)
+        bₖ₊₁′ = zero(bₖ₊₁)
+        for j = n-3:-1:1
+            @inbounds cⱼ = c[i1+j]
+            bⱼ = cⱼ + xd*(2bₖ) - bₖ₊₁
+            bⱼ′ = xd*(2bₖ′) + (2bₖ) - bₖ₊₁′
+            bₖ, bₖ₊₁ = bⱼ, bₖ
+            bₖ′, bₖ₊₁′ = bⱼ′, bₖ′
+        end
+        return c₁ + xd*bₖ - bₖ₊₁, hcat(SVector(bₖ + xd*bₖ′ - bₖ₊₁′))
+    else
+        Δi = len ÷ n # column-major stride of current dimension
+
+        # we recurse downward on dim for cache locality,
+        # since earlier dimensions are contiguous
+        dim′ = Val{dim-1}()
+
+        c₁,Jc₁ = Jinterpolate(x, c, dim′, i1, Δi)
+        if n ≤ 2
+            n == 1 && return c₁ + one(xd) * zero(c₁), hcat(Jc₁, SVector(zero(c₁) / oneunit(xd)))
+            c₂,Jc₂ = Jinterpolate(x, c, dim′, i1+Δi, Δi)
+            return c₁ + xd*c₂, hcat(Jc₁ + xd*Jc₂, SVector(c[i1]))
+        end
+        cₙ₋₁,Jcₙ₋₁ = Jinterpolate(x, c, dim′, i1+(n-2)*Δi, Δi)
+        cₙ,Jcₙ = Jinterpolate(x, c, dim′, i1+(n-1)*Δi, Δi)
+        bₖ = cₙ₋₁ + xd*(bₖ′ = 2cₙ)
+        Jbₖ = Jcₙ₋₁ + 2xd*Jcₙ
+        bₖ₊₁ = oftype(bₖ, cₙ)
+        bₖ₊₁′ = zero(bₖ₊₁)
+        Jbₖ₊₁ = oftype(Jbₖ, Jcₙ)
+        for j = n-3:-1:1
+            cⱼ,Jcⱼ = Jinterpolate(x, c, dim′, i1+j*Δi, Δi)
+            bⱼ = cⱼ + xd*(2bₖ) - bₖ₊₁
+            bⱼ′ = xd*(2bₖ′) + (2bₖ) - bₖ₊₁′
+            Jbⱼ = Jcⱼ + xd*(2Jbₖ) - Jbₖ₊₁
+            bₖ, bₖ₊₁ = bⱼ, bₖ
+            bₖ′, bₖ₊₁′ = bⱼ′, bₖ′
+            Jbₖ, Jbₖ₊₁ = Jbⱼ, Jbₖ
+        end
+        return c₁ + xd*bₖ - bₖ₊₁, hcat(Jc₁ + xd*Jbₖ - Jbₖ₊₁, SVector(bₖ + xd*bₖ′ - bₖ₊₁′))
+    end
+end
+
+
+"""
+    chebjacobian(c::ChebInterp, x)
+
+Return a tuple `(v, J)` where `v` is the value `c(x)` of the Chebyshev
+polynomial `c` at `x`, and `J` is the Jacobian of this value with respect to `x`.
+
+That is, if `v` is a vector, then `J` is a matrix of `length(v)` × `length(x)`
+giving the derivatives of each component.  If `v` is a scalar, then `J`
+is a 1-row matrix; in this case you may wish to call `chebgradient` instead.
+"""
+function chebjacobian(c::ChebInterp{N}, x::SVector{N,<:Real}) where {N}
+    x0 = @. (x - c.lb) * 2 / (c.ub - c.lb) - 1
+    all(abs.(x0) .≤ 1) || throw(ArgumentError("$x not in domain"))
+    v, J = Jinterpolate(x0, c.coefs, Val{N}(), 1, length(c.coefs))
+    return v, J .* 2 ./ (c.ub .- c.lb)'
+end
+
+chebjacobian(c::ChebInterp{N}, x::AbstractVector{<:Real}) where {N} = chebjacobian(c, SVector{N}(x))
+chebjacobian(c::ChebInterp{1}, x::Real) = chebjacobian(c, SVector{1}(x))
+
+"""
+    chebgradient(c::ChebInterp, x)
+
+Return a tuple `(v, ∇v)` where `v` is the value `c(x)` of the Chebyshev
+polynomial `c` at `x`, and `∇v` is the gradient of this value with respect to `x`.
+
+(Requires `c` to be a scalar-valued polynomial; if `c` is vector-valued, you
+should use `chebjacobian` instead.)
+
+If `x` is a scalar, returns a scalar `∇v`, i.e. `(v, ∂v/∂x).
+"""
+function chebgradient(c::ChebInterp, x)
+    v, J = chebjacobian(c, x)
+    length(v) == 1 || throw(DimensionMismatch())
+    return v, vec(J)
+end
+
+function chebgradient(c::ChebInterp{1}, x::Real)
+    v, ∇v = chebgradient(c, SVector{1}(x))
+    return v, ∇v[1]
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,26 +1,35 @@
-using Test, FastChebInterp
+using Test, FastChebInterp, StaticArrays
+
+# similar to ≈, but acts elementwise on tuples
+≈′(a::Tuple, b::Tuple; kws...) where {N} = length(a) == length(b) && all(xy -> isapprox(xy[1],xy[2]; kws...), zip(a,b))
 
 @testset "1d test" begin
     lb,ub = -0.3, 0.9
     f(x) = exp(x) / (1 + 2x^2)
+    f′(x) = f(x) * (1 - 4x/(1 + 2x^2))
     x = chebpoints(48, lb, ub)
     interp = chebinterp(f.(x), lb, ub)
     @test ndims(interp) == 1
     x1 = 0.2
     @test interp(x1) ≈ f(x1)
+    @test chebgradient(interp, x1) ≈′ (f(x1), f′(x1))
 end
 
 @testset "2d test" begin
     lb, ub = [-0.3,0.1], [0.9,1.2]
     f(x) = exp(x[1]+2*x[2]) / (1 + 2x[1]^2 + x[2]^2)
+    ∇f(x) = f(x) * SVector(1 - 4x[1]/(1 + 2x[1]^2 + x[2]^2), 2 - 2x[2]/(1 + 2x[1]^2 + x[2]^2))
     x = chebpoints((48,39), lb, ub)
     interp = chebinterp(f.(x), lb, ub)
     @test ndims(interp) == 2
     x1 = [0.2, 0.3]
     @test interp(x1) ≈ f(x1)
+    @test chebgradient(interp, x1) ≈′ (f(x1), ∇f(x1))
 
     # complex and vector-valued interpolants:
     f2(x) = [f(x), cis(x[1]*x[2] + 2x[2])]
+    ∇f2(x) = vcat(transpose(∇f(x)), transpose(SVector(im*x[2], im*(x[1] + 2)) * cis(x[1]*x[2] + 2x[2])))
     interp2 = chebinterp(f2.(x), lb, ub)
     @test interp2(x1) ≈ f2(x1)
+    @test chebjacobian(interp2, x1) ≈′ (f2(x1), ∇f2(x1))
 end
